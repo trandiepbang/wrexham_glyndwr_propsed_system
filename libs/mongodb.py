47baa58bd528a0df71b75c4848c01622521a66be
bangtran <bangtran@192.168.2.17>
2023-09-03 20:11:41 +1000

    update mongodb

--- libs/mongodb.py
+++ libs/mongodb.py
@@ -3,7 +3,7 @@ from mongoengine import connect
 
 def initMongoDB():
     # Extract database name from the URI
-    db_name = 'crime_db'    
+    db_name = os.environ.get('MONGO_DB', 'crime_db')
     #
     return connect(
         db=db_name,
